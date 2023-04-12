235c32bc5d69a3f0feaa8e2c2c94fe8f285a817c
snpatel <sh732170@dal.ca>
2023-04-11 23:37:33 -0300

    Changed backend link

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,10 @@
 import Axios from "axios";
 
+console.log("========= BACKEND IP =============");
+console.log(process.env.REACT_APP_BACKEND);
+
 const axios = Axios.create({
-    baseURL: "http://localhost:8003/api/v1",
+    baseURL: `${process.env.REACT_APP_BACKEND}/api/v1`,
     withCredentials: true,
 });
 
